73812404a99fbb2bbb0eaa8c727080da9e2ffc55
pkabus <peter.kabus@gmx.de>
2022-01-29 16:21:41 +0100

    NO-TICKET Just another bug (the master of computer science doesn't know how URLs are built correctly)

--- comunio-stats-frontend/src/backend_properties.js
+++ comunio-stats-frontend/src/backend_properties.js
@@ -1,7 +1,7 @@
 /* properties.js */
 
 const backend_properties = {
-    url: process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : "http://localhost:8080",
+    url: process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : "http://api-comuniohelper.ddns.net:8080"
 };
 
 export default backend_properties
